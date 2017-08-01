97a689c9ae16248407cb54078c331cfccb70503a
Ivar <ivar.chen@ge.com>
2017-08-01 09:58:12 +0800

    fixed rest client bug

--- basic/decorators.py
+++ basic/decorators.py
@@ -7,9 +7,9 @@ from basic import log
 def deco(fn):
     @wraps(fn)
     def wrapper(*args, **kwargs):
-        log.debug("before method call: %s with args: %s, desc: %s", fn.func_name, ','.join(args))
+        log.debug("before method call: %s with args: %s", fn.func_name, ','.join(args))
         msg = fn(*args, **kwargs)
-        log.debug("after method call: %s with args: %s, desc: %s", fn.func_name, ','.join(args))
+        log.debug("after method call: %s with args: %s", fn.func_name, ','.join(args))
         return msg
 
     return wrapper
@@ -22,27 +22,30 @@ def retry(attempts=3, after=1):
     def _decorate(fn):
         @wraps(fn)
         def wrapper(*args, **kwargs):
+            exception = None
             while temp_dict.get('retry_attempts') > 0:
                 try:
                     return fn(*args, **kwargs)
-                except:
+                except Exception as ex:
                     log.info('retry after %s seconds', temp_dict.get('retry_after'))
                     sleep(temp_dict.get('retry_after'))
                     temp_dict['retry_attempts'] -= 1
+                    exception = ex
+            raise RuntimeError('error: {} while running function {}()'.format(exception.message, fn.func_name))
 
         return wrapper
 
     return _decorate
 
 
-@deco(desc='this is a desc')
+@deco
 @retry()
 def hello(name):
     if name is not "Python":
-        raise Exception
+        raise Exception('not python error')
     return 'Hello, {}'.format(name)
 
 
 class DecoratorTest(unittest.TestCase):
     def test_deco(self):
-        hello('Pypy')
+        hello('python')
